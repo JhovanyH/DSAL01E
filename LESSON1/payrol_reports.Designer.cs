cdd9c1ec9dafa8d6773ee8ee6cc2e7e0e41804ad
Jhovany Honorio <2023-2-01030@lpunetwork.edu.ph>
2025-12-07 07:19:42 +0800

    payroll_reports

---
+++ LESSON1/payrol_reports.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LESSON1
+{
+    partial class payrol_reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.optionCombo = new System.Windows.Forms.ComboBox();
+            this.Back_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Search_button = new System.Windows.Forms.Button();
+            this.optionInputTxtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // optionCombo
+            // 
+            this.optionCombo.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionCombo.FormattingEnabled = true;
+            this.optionCombo.Items.AddRange(new object[] {
+            "employee_number",
+            "surname",
+            "firstname",
+            "gross_income",
+            "net_income",
+            "pay_date"});
+            this.optionCombo.Location = new System.Drawing.Point(265, 13);
+            this.optionCombo.Name = "optionCombo";
+            this.optionCombo.Size = new System.Drawing.Size(321, 35);
+            this.optionCombo.TabIndex = 11;
+            // 
+            // Back_button
+            // 
+            this.Back_button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Back_button.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Back_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Back_button.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back_button.Location = new System.Drawing.Point(1095, 10);
+            this.Back_button.Name = "Back_button";
+            this.Back_button.Size = new System.Drawing.Size(144, 41);
+            this.Back_button.TabIndex = 10;
+            this.Back_button.Text = "Back";
+            this.Back_button.UseVisualStyleBackColor = false;
+            this.Back_button.Click += new System.EventHandler(this.Back_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1471, 772);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Search_button
+            // 
+            this.Search_button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Search_button.BackColor = System.Drawing.Color.RosyBrown;
+            this.Search_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Search_button.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_button.Location = new System.Drawing.Point(936, 10);
+            this.Search_button.Name = "Search_button";
+            this.Search_button.Size = new System.Drawing.Size(144, 41);
+            this.Search_button.TabIndex = 8;
+            this.Search_button.Text = "Search";
+            this.Search_button.UseVisualStyleBackColor = false;
+            this.Search_button.Click += new System.EventHandler(this.Search_button_Click);
+            // 
+            // optionInputTxtbox
+            // 
+            this.optionInputTxtbox.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionInputTxtbox.Location = new System.Drawing.Point(602, 10);
+            this.optionInputTxtbox.Multiline = true;
+            this.optionInputTxtbox.Name = "optionInputTxtbox";
+            this.optionInputTxtbox.Size = new System.Drawing.Size(310, 41);
+            this.optionInputTxtbox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 27);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select an option:";
+            // 
+            // payrol_reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1495, 849);
+            this.Controls.Add(this.optionCombo);
+            this.Controls.Add(this.Back_button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Search_button);
+            this.Controls.Add(this.optionInputTxtbox);
+            this.Controls.Add(this.label1);
+            this.Name = "payrol_reports";
+            this.Text = "payrol_reports";
+            this.Load += new System.EventHandler(this.payrol_reports_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox optionCombo;
+        private System.Windows.Forms.Button Back_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Search_button;
+        private System.Windows.Forms.TextBox optionInputTxtbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LESSON1/payrol_reports.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LESSON1
+{
+    public partial class payrol_reports : Form
+    {
+        payrol_dbconnection pd = new payrol_dbconnection();
+
+        public payrol_reports()
+        {
+            pd.payrol_connString();
+            InitializeComponent();
+        }
+
+        private void payrol_select()
+        {
+            pd.payrol_cmd();
+            pd.payrol_sqladapterSelect();
+            pd.payrol_sqldatasetSELECT();
+            dataGridView1.DataSource = pd.payrol_sql_dataset.Tables[0];
+        }
+
+        private void cleartextboxes()
+        {
+            optionCombo.Text = "";
+            optionInputTxtbox.Clear();
+            optionCombo.Focus();
+        }
+
+        private void cleartextboxes1()
+        {
+            optionInputTxtbox.Clear();
+            optionInputTxtbox.Focus();
+        }
+        private void payrol_reports_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id";
+
+                payrol_select();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error occurs in this area. Please contact your administrator!");
+            }
+        }
+
+        private void Search_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (optionCombo.Text == "employee_number")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE payrolTbl.emp_id = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else if (optionCombo.Text == "surname")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE pos_empRegTbl.emp_surname = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else if (optionCombo.Text == "firstname")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE pos_empRegTbl.emp_fname = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else if (optionCombo.Text == "gross_income")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE payrolTbl.gross_income = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else if (optionCombo.Text == "net_income")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE payrolTbl.net_income = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else if (optionCombo.Text == "pay_date")
+                {
+                    pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                    "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                    "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                    "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                    "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                    "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                    "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                    "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id " +
+                    "WHERE payrolTbl.pay_date = '" + optionInputTxtbox.Text + "'";
+
+                    payrol_select();
+                    cleartextboxes1();
+                }
+                else
+                {
+                    MessageBox.Show("No Available Record Found!");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error occurs in this area. Please contact your administrator!");
+            }
+        }
+
+        private void Back_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pd.payrol_sql = "SELECT emp_fname, emp_mname, emp_surname, " +
+                "basic_rate_hr, basic_no_of_hrs_cutoff, basic_income_per_cutoff, " +
+                "honorarium_rate_hr, honorarium_no_of_hrs_cutoff, honorarium_income_per_cutoff, " +
+                "other_rate_hr, other_no_of_hrs_cutoff, other_income_per_cutoff, " +
+                "sss_contrib, philhealth_contrib, pagibig_contrib, tax_contrib, " +
+                "sss_loan, pagibig_loan, fac_savings_deposit, fac_savings_loan, " +
+                "salary_loan, other_loans, total_deductions, gross_income, net_income, pay_date " +
+                "FROM pos_empRegTbl INNER JOIN payrolTbl ON pos_empRegTbl.emp_id = payrolTbl.emp_id";
+
+                payrol_select();
+                cleartextboxes();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error occurs in this area. Please contact your administrator!");
+            }
+        }
+    }
+}
